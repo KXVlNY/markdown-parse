b724971ed4eb0a7c5ef1f1d5871448b045290675
KXVlNY <97643580+KXVlNY@users.noreply.github.com>
2022-01-26 18:32:20 -0800

    Run JUnit Tester

---
+++ MarkdownParseTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1); // Test addition methods and make sure it returns 2
+    }
+}
