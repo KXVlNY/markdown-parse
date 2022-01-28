5f49d41f5ed1a2075e870c49b357efdb45f050bb
KXVlNY <97643580+KXVlNY@users.noreply.github.com>
2022-01-27 21:44:38 -0800

    JUnit Tester for MarkdownParse.java
    still not working

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,18 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1); // Test addition methods and make sure it returns 2
+    }
+    public void getLinks() {
+        Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = getLinks(contents);
+
     }
 }
